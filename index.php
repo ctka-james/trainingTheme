4b5a27ea10097e519ef02c02dfa3ae734805ca47
JamesNB <james@u-sen.com.tw>
2022-07-01 07:57:08 +0800

    poedit training

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php get_header(); ?>
 <hr>
-
+<table>
+    <tr>
+        <th><?php _e('翻譯中文字', 'trainingTheme'); ?></th>
+    </tr>
+</table>
 <?php while (have_posts()) : the_post(); ?>
     <p>
         <?php the_title(); ?>
